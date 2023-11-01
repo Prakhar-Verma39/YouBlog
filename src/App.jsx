2041c50ed0768f2dbb4e5fc88e695a6f527d91da
Prakhar-Verma39 <103757447+Prakhar-Verma39@users.noreply.github.com>
2023-11-02 02:58:35 +0530

    UI correction

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ function App() {
     .finally(() => setLoading(false))
   }, [])
   return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-      <div className='w-full block'>
+    <div className='min-h-screen flex flex-wrap justify-center bg-gray-400'>
+      <div className='w-full flex flex-col justify-between flex-grow'>
         <Header/>
         <main>
         <Outlet />
