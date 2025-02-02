c765dd1a78917343f4ecf73a4282c25fbbb9e938
Prakhar-Verma39 <103757447+Prakhar-Verma39@users.noreply.github.com>
2025-02-02 23:33:08 +0530

    style: change header styles and button styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -35,35 +35,42 @@ function Header () {
             active: authStatus,
         },
     ]
-    return (
-        <header className="py-3 shadow font-mono font-bold bg-SecondaryColor rounded-3xl">
-            <Container>
-                <nav className="flex">
-                    <div className="mr-4">
-                        <Link to="/">
-                            <Logo width='70px'/>
-                        </Link>
-                    </div>
-                    <ul className="flex ml-auto">
-                        {navItems.map((item) => 
-                        item.active ? (
-                            <li key={item.name}>
-                                <button
-                                onClick={() => navigate(item.slug)}
-                                className="inline-block px-6 py-2 duration-200 hover:bg-blue-100
-                                rounded-full"
-                                >
-                                    {item.name}</button>
-                            </li>
-                        ) : null)}
-                        {authStatus && (
-                            <li>
-                                <LogoutBtn/>
-                            </li>
-                        )}
-                    </ul>
-                </nav>
-            </Container>
+    return ( 
+        <header className='relative font-mono font-bold'>
+                <Container>
+                    <span className="absolute top-0 left-0 w-[53%] h-full -z-10 bg-[linear-gradient(152deg,#fff_25%,#ffffff00_55%)] rounded-md"></span>            
+                    <nav className="flex items-center">
+                        <div>
+                            <Link to="/">
+                                <Logo width='70px'/>
+                            </Link>
+                        </div>
+                        <ul className="flex gap-10 ml-auto mr-12">
+                            {navItems.map((item) => 
+                            item.active ? (
+                                <li key={item.name}>
+                                    <button
+                                    onClick={() => navigate(item.slug)}
+                                    className="relative inline-block px-4 py-2 font-medium group"
+                                    >
+                                        <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                                        <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                                        <span class="relative text-black group-hover:text-white">
+                                            {item.name}
+                                        </span>
+                                    </button>
+                                    
+                                </li>
+                            ) : null)}
+                            {authStatus && (
+                                <li>
+                                    <LogoutBtn/>
+                                </li>
+                            )}
+                        </ul>
+                    </nav>
+                </Container>
+            
         </header>
     )
 }
--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -12,10 +12,15 @@ function LogoutBtn () {
     }
     return (
         <button
-        className="inline-block px-6 py-2 duration-200
-        hover:bg-blue-100 rounded-full"
+        className="relative inline-block px-4 py-2 font-medium group"
         onClick={logoutHandler}
-        >Logout</button>
+        >
+            <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+            <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+            <span class="relative text-black group-hover:text-white">   
+                Logout
+            </span> 
+        </button>
     )
 }
 
